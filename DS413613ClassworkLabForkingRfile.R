14ff536e5f2ae1429293f3d9630531be42839fcf
Lucy <lucy.bonin@gmail.com>
2022-03-16 18:02:32 -0400

    Update DS413613ClassworkLabForkingRfile.R

--- DS413613ClassworkLabForkingRfile.R
+++ DS413613ClassworkLabForkingRfile.R
@@ -2,15 +2,15 @@ library(tidyverse)
 library(ggplot2)
 library(dplyr)
 mpg
-# My name is Dennis P.
-#The data source is the mpg data table
-# This is my coding that generates Boxplots for transmission types
+# My name is Lucy Bonin
+#The data source is the mpg data table from R
+#Code for producing boxplots
 ggplot(data = mpg) +
   geom_boxplot(mapping = aes(x = drv, y = displ, fill = drv)) +
   ggtitle("Boxplots (mpg data : transmission types vs displacement)") +
   xlab("transmission types") +
   ylab("displacement")+
   coord_flip()
-  
+  #how it is flipped to be horizontal
 
 
